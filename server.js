f18c730ca30ec850d0b6100797c185285a66173d
Kimgahyeon <kimgahyun1@ajou.ac.kr>
2019-07-16 23:07:05 +0900

    partial/css 분리, ejs 뷰템플릿 적용

--- server.js
+++ server.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const app = express();
 var bodyParser = require('body-parser');
+
 // Constants
 const PORT = 8080;
-const HOST = '10.0.2.15';
-
+const HOST = "127.0.0.1";
 
  // Hyperledger Bridge
 const { FileSystemWallet, Gateway } = require('fabric-network');
@@ -16,19 +16,24 @@ const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ejs view template
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Index page
 app.get('/', function (req, res) {
-  fs.readFile('./application/index.html', function (error, data) {
-              res.send(data.toString());
-
-  });
+    res.render('index', { title: "Main Page", activate: "index"});
 });
 
 // Qeury all cars page
-app.get('/api/query', async function (req, res) {
-		// create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+app.get('/api/query', function (req, res) {
+    res.render('query', { title: "Query", activate: "query" });
+});
 
+app.get('/api/query', async function (req, res) {
+     // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
         // Create a new file system based wallet for managing identities.
         const walletPath = path.join(process.cwd(), 'wallet');
         const wallet = new FileSystemWallet(walletPath);
@@ -42,30 +47,31 @@ app.get('/api/query', async function (req, res) {
             return;
         }
 
-        // Create a new gateway for connecting to our peer node.
         const gateway = new Gateway();
         await gateway.connect(ccp, { wallet, identity: 'user1', discovery: { enabled: false } });
 
-        // Get the network (channel) our contract is deployed to.
-        const network = await gateway.getNetwork('mychannel');
 
-        // Get the contract from the network.
+        const network = await gateway.getNetwork('mychannel');
         const contract = network.getContract('fabcar');
 
-        // Evaluate the specified transaction.
-        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
-        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
         const result = await contract.evaluateTransaction('queryAllCars');
         console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
 
-		res.status(200).json({response: result.toString()});
-	    res.end(JSON.stringify(result));
+        var obj = JSON.parse(result)
+        res.json(obj)
+
+        // FIXME:
+        // res.status(200).json({response: result.toString()});
+        // res.end(JSON.stringify(result));
+
+        // res.status(200).send(obj);
+        // res.end(JSON.stringify(result));
 });
 
 // Query car handle
 // localhost:8080/api/querycar?carno=CAR5
 app.get('/api/querycar/', async function (req, res) {
-                // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
+    // create the key value store as defined in the fabric-client/config/default.json 'key-value-store' setting
     try {
 	var carno = req.query.carno;
 	console.log(carno);
@@ -106,9 +112,7 @@ app.get('/api/querycar/', async function (req, res) {
 
 // Create car page
 app.get('/api/createcar', function (req, res) {
-  fs.readFile('./application/createcar.html', function (error, data) {
-              res.send(data.toString());
-  });
+    res.render('createcar', { title: "Create Car", activate: "createcar"  });
 });
 // Create car handle
 app.post('/api/createcar/', async function (req, res) {
@@ -144,7 +148,7 @@ app.post('/api/createcar/', async function (req, res) {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-//        await contract.submitTransaction('createCar', 'CAR11', 'Hnda', 'Aord', 'Bla', 'Tom');
+        //await contract.submitTransaction('createCar', 'CAR11', 'Hnda', 'Aord', 'Bla', 'Tom');
         await contract.submitTransaction('createCar', carno, make, model, colour, owner);
         console.log('Transaction has been submitted');
 
@@ -162,10 +166,7 @@ app.post('/api/createcar/', async function (req, res) {
 
 // Change car owner page
 app.get('/api/changeowner', function (req, res) {
-  fs.readFile('./application/changeowner.html', function (error, data) {
-              res.send(data.toString());
-
-  }); 
+    res.render('changeowner', { title: "Change Owner", activate: "changeowner" });
 });
 
 // Change car owner handle
@@ -200,7 +201,7 @@ app.post('/api/changeowner/', async function (req, res) {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR10', 'Dave')
-//        await contract.submitTransaction('createCar', 'CAR11', 'Hnda', 'Aord', 'Bla', 'Tom');
+        //await contract.submitTransaction('createCar', 'CAR11', 'Hnda', 'Aord', 'Bla', 'Tom');
         await contract.submitTransaction('changeCarOwner', carno, owner);
         console.log('Transaction has been submitted');
 
@@ -212,10 +213,8 @@ app.post('/api/changeowner/', async function (req, res) {
         console.error(`Failed to submit transaction: ${error}`);
         res.status(400).json(error);
     }   
-
 });
 
 // server start
 app.listen(PORT, HOST);
 console.log(`Running on http://10.0.2.15:${PORT}`);
-
